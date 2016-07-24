b1a773deae2c2fa97f079dc616767ac1c99b3b56
Lucas Garcia <lucas.garcia@mudano.com>
2016-07-24 12:23:57 -0300

    UI Router example

---
+++ public/js/app.js
@@ -0,0 +1,39 @@
+var app = angular.module('ui-router-example', ['ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('workspace', {
+            url: "/workspace",
+            templateUrl: '/templates/workspace.html'
+        })
+        .state('workspace.to_do', {
+            url: "/to_do",
+            templateUrl: '/templates/to_do.html'
+        })
+        .state('workspace.to_do.task', {
+            url: "/task/:taskID",
+            views: {
+                to_do: {
+                    templateUrl: '/templates/task.html'
+                }
+            }
+        })
+        .state("workspace.to_do.event", {
+            url: "/event/:eventID",
+            views: {
+                to_do: {
+                    templateUrl: '/templates/event.html'
+                }
+            }
+        })
+        .state('workspace.to_do.event.details', {
+            url: "/details",
+            views: {
+                details: {
+                    templateUrl: '/templates/details.html'
+                }
+            }
+        });
+
+    $urlRouterProvider.otherwise("/workspace");
+});
